fd22f03872d65894f5bab174a2dcd120d5261700
James Bridle <james@shorttermmemoryloss.com>
2023-10-31 10:40:57 +0200

    Transparency
    Added composition of black image to render transparent / night areas as black correctly

--- weatherdesk-2.0-aegean.sh
+++ weatherdesk-2.0-aegean.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-wget -O source.png 'https://view.eumetsat.int/geoserver/ows?service=WMS&request=GetMap&version=1.3.0&layers=msg_fes:rgb_naturalenhncd&styles=&format=image/jpeg&crs=EPSG:4326&bbox=-77,-77,77,77&width=4000&height=4000'
+wget -O source.png 'https://view.eumetsat.int/geoserver/ows?service=WMS&request=GetMap&version=1.3.0&layers=msg_fes:rgb_naturalenhncd&styles=&format=image/png&transparent=true&crs=EPSG:4326&bbox=-77,-77,77,77&width=4000&height=4000'
 convert source.png -crop 638x384+2290+842 cropped_source.png
 convert -size 1276x768 cropped_source.png -magnify -unsharp 0x1 cropped_source.png
-dconf write "/org/gnome/desktop/background/picture-uri" "'file:///[PATH_TO_DIRECTORY]/weatherdesk/cropped_source.png'"
+composite cropped_source.png black.jpg satellite.jpg
+dconf write "/org/gnome/desktop/background/picture-uri" "'file:///home/james/Dropbox/Code/weatherdesk/satellite.jpg'"
 exit 0
--- weatherdesk-2.0-mediterranean.sh
+++ weatherdesk-2.0-mediterranean.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-wget -O source.png 'https://view.eumetsat.int/geoserver/ows?service=WMS&request=GetMap&version=1.3.0&layers=msg_fes:rgb_naturalenhncd&styles=&format=image/jpeg&crs=EPSG:4326&bbox=-77,-77,77,77&width=4000&height=4000'
-convert source.png -crop 1276x768+1736+624 cropped_source.png
-dconf write "/org/gnome/desktop/background/picture-uri" "'file:///[PATH_TO_DIRECTORY]/weatherdesk/cropped_source.png'"
+wget -O source.png 'https://view.eumetsat.int/geoserver/ows?service=WMS&request=GetMap&version=1.3.0&layers=msg_fes:rgb_naturalenhncd&styles=&format=image/png&transparent=true&crs=EPSG:4326&bbox=-77,-77,77,77&width=4000&height=4000'
+convert source.png -crop 1276x768+1875+633 cropped_source.png
+composite cropped_source.png black.jpg satellite.jpg
+dconf write "/org/gnome/desktop/background/picture-uri" "'file:///home/james/Dropbox/Code/weatherdesk/satellite.jpg'"
 exit 0
